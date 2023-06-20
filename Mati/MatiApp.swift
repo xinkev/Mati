8f6099328bf99d7501c4fb68eaa214a1c1f90a71
xinkev <lyx13710@gmail.com>
2023-06-20 21:22:30 +0630

    Initial Commit

---
+++ Mati/MatiApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatiApp.swift
+//  Mati
+//
+//  Created by xinkev on 20/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MatiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
