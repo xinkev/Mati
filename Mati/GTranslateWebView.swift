04f0a354cbc22063c287f60b4f4e6cb72196c3cc
xinkev <lyx13710@gmail.com>
2023-06-22 00:09:46 +0630

    Remove website's navbar

--- Mati/GTranslateWebView.swift
+++ Mati/GTranslateWebView.swift
@@ -14,15 +14,43 @@ struct GTranslateWebView: NSViewRepresentable {
     func makeNSView(context: Context) -> WKWebView {
         let preferences = WKWebpagePreferences()
         preferences.allowsContentJavaScript = true
-        let configurations = WKWebViewConfiguration()
-        configurations.defaultWebpagePreferences = preferences
         
-        return WKWebView(frame: CGRect.zero, configuration: configurations)
+        let configuration = WKWebViewConfiguration()
+        configuration.defaultWebpagePreferences = preferences
+        
+        let webView = WKWebView(frame: CGRect.zero, configuration: configuration)
+        webView.navigationDelegate = context.coordinator // Set the navigation delegate
+        webView.isHidden = true
+        return webView
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
     }
     
     func updateNSView(_ webView: WKWebView, context: Context) {
         let request = URLRequest(url: url)
         webView.load(request)
+    }
+    
+    class Coordinator: NSObject, WKNavigationDelegate {
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            // Inject JavaScript to remove <header> tag
+            let jsCode = """
+                    var header = document.querySelector('header');
+                    if (header) {
+                        header.remove();
+                    }
+                    var whiteSpace = document.querySelector(".VjFXz");
+                    if (whiteSpace) {
+                        whiteSpace.remove();
+                    }
+                    """
+            webView.evaluateJavaScript(jsCode) { _, _ in
+                // After JavaScript injection is complete, show the webpage
+                webView.isHidden = false
+            }
+        }
     }
 }
 
